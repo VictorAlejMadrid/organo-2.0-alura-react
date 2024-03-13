6d88caa9a28621df9ae75a105c7be6acc29be215
Victor Madrid <victoralej.madrid@gmail.com>
2024-03-13 17:48:18 -0300

    Adição dos arquivos

---
+++ src/componentes/Campo/index.js
@@ -0,0 +1,17 @@
+import { typeImplementation } from '@testing-library/user-event/dist/type/typeImplementation'
+import './campo.css'
+
+const Campo = ({ type = 'text', label, placeholder, valor, aoAlterado, obrigatorio = false }) => {
+    return (<div className={`campo campo-${type}`}>
+        <label>{label}</label>
+        <input
+            type={type} 
+            value={valor} 
+            onChange={evento => aoAlterado(evento.target.value)} 
+            required={obrigatorio} 
+            placeholder={placeholder}
+        /> 
+    </div>)
+}
+
+export default Campo
